d07ec21fdffa6a757c3e8ea6db902bc34e34d6d6
Toluvia Ruby <truby@ge-l4.kzn.21-school.ru>
2021-05-30 18:30:39 +0300

    writing sort for export

--- truby/export.c
+++ truby/export.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 
-static char *find_next_lines(char *previous, char **env)
+static char *find_next_line(char *previous, char **env)
 {
 	char *next;
 	int i = -1;
 	int j = 0;
+	int	fl = 0;
 
 	while (env[++i] != NULL)
 	{
@@ -21,9 +22,10 @@ static char *find_next_lines(char *previous, char **env)
 			while (env[i][j] && previous[j])
 			{
 				if (env[i][j] > previous[j])
+				{
 					next = env[i];
-				if (env[i][j] < previous[j])
 					break;
+				}
 				j++;
 			}
 			j = 0;
@@ -32,45 +34,170 @@ static char *find_next_lines(char *previous, char **env)
 	i = -1;
 	while (env[++i] != NULL)
 	{
+		// write(1, next, 15);
+		// write(1, "\n", 1);
 		if (env[i][0] > previous[0] && env[i][0] < next[0])
 		{
 			next = env[i];
-			i = 0;
 		}
 		else if (env[i][0] == previous[0])
 		{
-			while (env[i][j] && previous[j])
+			if (env[i][0] != next[0])
 			{
-				if (env[i][j] > previous[j] && env[i][j] < next[j])
-					break;
-				if (env[i][j] < previous[j])
+				while (env[i][j] && previous[j])
+				{
+					if (env[i][j] > previous[j])
+					{
+						next = env[i];
+						fl = 1;
+						break;
+					}
+					else if (env[i][j] < previous[j])
+						break;
+					j++;
+				}
+				j = 0;
+			}
+			else if (env[i][0] == next[0] && !fl)
+			{
+				// write(1, next, 15);
+				// write(1, "\n", 1);
+				while (env[i][j] && next[j])
+				{
+					if (env[i][j] > next[j]) //&& env[i][j] > previous[j])
+					{
+						next = env[i];
+						break;
+					}
+					else if (env[i][j] < next[j])
+						break;
+					j++;
+				}
+				j = 0;
+			}
+			else if (env[i][0] == next[0] && fl)
+			{
+				// write(1, env[i], 15);
+				// write(1, "ii", 2);
+				// write(1, "\n", 1);
+				while (env[i][j] && next[j])
+				{
+					if (env[i][j] < next[j] && env[i][j] > previous[j])
+					{
+						next = env[i];
+						// fl = 0;
+						break;
+					}
+					// else if (env[i][j] > next[j] && env[i][j] == previous[j])
+					// {
+					// 	next = env[i];
+					// 	break;
+					// }
+					else if (env[i][j] > next[j])
+						break;
+					j++;
+				}
+				j = 0;
+			}
+		}
+		else if (env[i][0] > previous[0] && env[i][0] == next[0])
+		{
+			while (env[i][j] && next[j])
+			{
+				if (env[i][j] < next[j])
+				{
 					next = env[i];
+					break;
+				}
+				else if (env[i][j] > next[j])
+					break;
 				j++;
 			}
 			j = 0;
-		}		
+		}
+		// else if (env[i][0] == previous[0] && env[i][0] != next[0])
+		// {
+		// 	while (env[i][j] && previous[j])
+		// 	{
+		// 		if (env[i][j] > previous[j])
+		// 		{
+		// 			next = env[i];
+		// 			break;
+		// 		}
+		// 		else if (env[i][j] < previous[j])
+		// 			break;
+		// 		j++;
+		// 	}
+		// 	j = 0;
+		// }
+		// else if (env[i][0] == previous[0] && env[i][0] == next[0])
+		// {
+		// 	// write(1, env[i], 6);
+		// 	// write(1, "\n", 1);
+		// 	while (env[i][j] && next[j])
+		// 	{
+		// 		if (env[i][j] > previous[j])
+		// 		{
+		// 			if (env[i][j] < next[j])
+		// 			{
+		// 				next = env[i];
+		// 				break;
+		// 			}
+		// 			else if (env[i][j] > next[j])
+		// 			{
+
+		// 			}
+		// 			// write(1, next, 6);
+		// 			// write(1, "\n", 1);
+		// 		}
+		// 		else if (env[i][j] > next[j] && env[i][j] != previous[j])
+		// 			break;
+		// 		j++;
+		// 	}
+		// 	j = 0;
+			// write(1, next, 6);
+			// write(1, "\n", 1);
+		// }
+		// else if (env[i][0] != previous[0] && env[i][0] == next[0])
+		// {
+			// write(1, next, 6);
+			// write(1, "\n", 1);
+			// while (env[i][j] && next[j])
+			// {
+			// 	if (env[i][j] < next[j] && env[i][j] != previous[j])
+			// 	{
+			// 		next = env[i];
+					// write(1, next, 6);
+					// write(1, "\n", 1);
+					// break;
+			// 	}
+			// 	else if (env[i][j] > next[j])
+			// 		break;
+			// 	j++;
+			// }
+			// j = 0;
+			// write(1, next, 6);
+			// write(1, "\n", 1);
+		// }
 	}
+	// write(1, next, 6);
+	// write(1, "\n", 1);
 	return (next);
 }
 
-static char	**find_first_line(char **env)
+static char	*find_first_line(char **env)
 {
-	int i = 0;
+	int i = -1;
 	int j = 1;
-	char **export;
 	char *first;
 
-	while (env[i] != NULL)
-		i++;
-	export = malloc(sizeof(char*) * (i + 1));
-	i = -1;
 	first = env[0];
 	while (env[++i] != NULL)
 	{
 		if (env[i][0] < first[0])
 		{
 			first = env[i];
-			i = -1;
+			// i = -1;
 		}
 		else if (env[i][0] == first[0])
 		{
@@ -88,22 +215,106 @@ static char	**find_first_line(char **env)
 			j = 0;
 		}
 	}
-	printf("%s", first);
-	printf("%s", srt(first, env));
-	free(export);
-	return (export);
+	return (first);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	int i = -1;
-	char **export;
+	char *line;
 
-	export = find_first_line(env);
-	// while (export[++i] != NULL)
+	line = find_first_line(env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	line = find_next_line(line, env);
+	write(1, line, strlen(line));
+	write(1, "\n", 1);
+	// while (line != NULL)
 	// {
-	// 	write(1, export[i], strlen(export[i]));
 	// 	write(1, "\n", 1);
+	// 	write(1, line, strlen(line));
+	// 	line = find_next_line(line, env);
 	// }
 	return (0);
 }
