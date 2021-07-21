250ffcd0f2dc1d838f2039d82cde9cd040d013fd
Toluvia Ruby <truby@et-h2.kzn.21-school.ru>
2021-07-21 23:22:47 +0300

    Continued working on norminette. Two files left.

--- truby/extra_functions2.c
+++ truby/extra_functions2.c
@@ -6,11 +6,51 @@
 /*   By: truby <truby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 15:34:14 by truby             #+#    #+#             */
-/*   Updated: 2021/07/21 22:26:33 by truby            ###   ########.fr       */
+/*   Updated: 2021/07/21 23:21:13 by truby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell_truby.h"
+
+static void	close_dup(t_all *command)
+{
+	if (command->fd_out != 1)
+	{
+		close(1);
+		dup2(command->fd_out, 1);
+	}
+	if (command->fd_in != 0)
+	{
+		close(0);
+		dup2(command->fd_in, 0);
+	}	
+}
+
+void	fork_exec(t_all *command, t_env *env, int fl)
+{
+	pid_t	p;
+	int		res;
+	int		fd_0;
+	int		fd_1;
+
+	fd_0 = dup(0);
+	fd_1 = dup(1);
+	close_dup(command);
+	if (fl == 0)
+	{
+		p = fork();
+		if (p < 0)
+			return (ft_error("Fork failed.", errno));
+		else if (p == 0)
+			implementation(command, env);
+		wait(&res);
+		dup2(fd_0, 0);
+		dup2(fd_1, 1);
+		g_status = res / 256;
+	}
+	else
+		implementation(command, env);
+}
 
 int	validation(char *arg_str)
 {
@@ -46,24 +86,20 @@ char	**create_massiv(t_env *env)
 
 	i = 0;
 	first = env;
-	while (env->str)
+	while (env != NULL)
 	{
 		i++;
-		if (env->next == NULL)
-			break ;
 		env = env->next;
 	}
 	env_massiv = malloc(sizeof(char *) * (i + 1));
 	if (!env_massiv)
 		return (ft_error_null("Error of malloc.", ENOMEM));
 	i = -1;
-	while (first->str)
+	while (first != NULL)
 	{
 		env_massiv[++i] = ft_strdup(first->str);
 		if (!env_massiv[i])
 			return (ft_error_null("Error of malloc.", ENOMEM));
-		if (first->next == NULL)
-			break ;
 		first = first->next;
 	}
 	env_massiv[i + 1] = NULL;
@@ -92,39 +128,4 @@ void	free_massives(char **paths, char **env_massiv, char *pwd, int fl)
 	free(paths);
 	free(env_massiv);
 	free(pwd);
-}
-
-void	fork_exec(t_all *command, t_env *env, int fl)
-{
-	pid_t	p;
-	int		res;
-	int		fd_0;
-	int		fd_1;
-
-	fd_0 = dup(0);
-	fd_1 = dup(1);
-	if (command->fd_out != 1)
-	{
-		close(1);
-		dup2(command->fd_out, 1);
-	}
-	if (command->fd_in != 0)
-	{
-		close(0);
-		dup2(command->fd_in, 0);
-	}	
-	if (fl == 0)
-	{
-		p = fork();
-		if (p < 0)
-			return (ft_error("Fork failed.", errno));
-		else if (p == 0)
-			implementation(command, env);
-		wait(&res);
-		dup2(fd_0, 0);
-		dup2(fd_1, 1);
-		g_status = res / 256;
-	}
-	else
-		implementation(command, env);
 }
